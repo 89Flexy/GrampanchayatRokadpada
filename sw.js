5b577013eeeebb3608155790b445e9f9e551ef87
Abhi <factsstorage42@gmail.com>
2025-12-25 15:53:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,3 +7,11 @@ self.addEventListener("activate", () => {
 });
 
 self.addEventListener("fetch", () => {});
+
+<script>
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(() => console.log('SW registered'))
+    .catch(console.error);
+}
+</script>
